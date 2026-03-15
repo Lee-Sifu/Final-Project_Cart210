6db903fe24050cab8987b4dcc3412a848e6fa0c0
Jason Lee <jason.m.lee.2003@gmail.com>
2026-03-14 21:01:07 -0400

    Added mouse press function to grow plants

--- js/script.js
+++ js/script.js
@@ -1,7 +1,40 @@
+let plants = [];
+
 function setup() {
     createCanvas(400, 400);
 }
 
 function draw() {
     background("#87ceeb");
+    for (let i = 0; i < plants.length; i++) {
+        plants[i].grow();
+        plants[i].display();
+    }
+}
+
+function mousePressed() {
+    let newPlant = new Plant(mouseX, mouseY);
+    plants.push(newPlant);
+}
+
+class Plant {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 1;
+    this.maxSize = random(20, 80);
+    this.color = color(random(100,255), random(150,255), random(100,255));
+  }
+
+    grow() {
+        if (this.size < this.maxSize) {
+            this.size += 0.5;
+        }
+    }
+
+    display() {
+        fill(this.color);
+        noStroke();
+        ellipse(this.x, this.y, this.size, this.size);
+    }
 }
