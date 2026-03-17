2952f46b353b53d7fdf18b4ad4bc2cb5ce32527c
Jason Lee <jason.m.lee.2003@gmail.com>
2026-03-17 16:24:30 -0400

    added trees

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let plants = [];
+let trees = [];
 
 function setup() {
     createCanvas(400, 400);
@@ -8,13 +9,21 @@ function draw() {
     background("#1e541f");
     for (let i = 0; i < plants.length; i++) {
         plants[i].grow();
-        plants[i].display();
+        plants[i].display();  
+    }
+    for (let i = 0; i < trees.length; i++) {
+        trees[i].treegrow();
+        trees[i].treedisplay();
     }
 }
 
 function mousePressed() {
     let newPlant = new Plant(mouseX, mouseY);
     plants.push(newPlant);
+    if (plants.length % 8 === 0) {
+        let newTree = new Tree(mouseX, mouseY);
+        trees.push(newTree);
+    }
 }
 
 class Plant {
@@ -44,5 +53,31 @@ class Plant {
     noStroke();
     fill(this.color);
     circle(this.x, this.y - this.size, this.size/2);
+  }
+}
+
+class Tree {
+  constructor(x, y) {
+    this.x = x; 
+    this.y = y;
+    this.size = 1;
+    this.maxSize = random(50, 150);
+    this.color = color(random(50,150), random(100,200), random(50,150));
+  }
+  treegrow() {
+    if (this.size < this.maxSize) {
+        this.size += 0.02;
+        this.size = constrain(this.size, 0, this.maxSize);
+    }
+}
+    treedisplay() {
+    stroke(this.color);
+    strokeWeight(4);
+    // trunk
+    line(this.x, this.y, this.x, this.y - this.size);
+    // leaves
+    noStroke();
+    fill(this.color);
+    circle(this.x, this.y - this.size, this.size);
   }
 }
